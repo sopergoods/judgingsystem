d755bcedd0431b64c2c047205f20e3dd99d4dd8b
Vince Timothy Asutilla <asutillavince@gmail.com>
2025-11-12 18:50:34 +0800

    fix

--- judge-app.js
+++ judge-app.js
@@ -1019,7 +1019,7 @@ function startLockCountdown(judgeId, participantId, competitionId, segmentId, sc
         lockCountdown--;
         updateLockCountdown();
         
-        if (lockCountdown <= 10) {
+        if (lockCountdown <= 0) {
             clearInterval(lockTimer);
             autoLockScore();
         }
